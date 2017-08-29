45498c954112dee3cdbdb2dacf0232056e191bfc
terenace <terence.5816@gmail.com>
2017-08-30 03:20:47 +0800

    8/29 node testing and add music

--- catkin_ws/src/summer_school/qwer_nutn/socket_qwer/src/print.py
+++ catkin_ws/src/summer_school/qwer_nutn/socket_qwer/src/print.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 import time
 import rospy
+import pygame
 class qwer_print(object):
     def __init__(self):
         for i in range (3):
             rospy.loginfo('****************TEST*********** %f' % i)
+        self.music()
+
+    def music(self):
+        sound = '1234.mp3'
+        pygame.mixer.init()
+        pygame.mixer.music.load(sound)
+        pygame.mixer.music.set_volume(0.05)  #The value argument is between 0.0 and 1.0.
+        pygame.mixer.music.play(0, 0) #1st number set loops, 2nd controls where in the music the song starts playing.
+        print('-------IN def music(self)-------------')
+        while pygame.mixer.music.get_busy(): 
+            pygame.time.Clock().tick(10)
 
 if __name__ == '__main__':
     rospy.init_node("qwer_print",anonymous=False)
